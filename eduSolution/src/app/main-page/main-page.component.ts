32125c9424dd61dde4a23d12ab70b96e13d7e170
MichalinaTokarek <michalina.tokarek@gmail.com>
2024-01-24 16:31:22 +0100

    Strona główna poczatek i kolory główne

--- eduSolution/src/app/main-page/main-page.component.ts
+++ eduSolution/src/app/main-page/main-page.component.ts
@@ -7,6 +7,7 @@ import { Router } from '@angular/router';
 @Component({
   selector: 'app-main-page',
   templateUrl: './main-page.component.html',
+  styleUrls: ['main-page.component.css']
 
 })
 export class MainPageComponent implements OnInit{
